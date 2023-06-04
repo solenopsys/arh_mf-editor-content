4f6206a7792bbf1f424ea1ff4d8e069a4bb8211f
AlexStorm <info@alexstorm.ru>
2023-06-04 18:46:47 +0530

    deep refactoring monorepo, remove nx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import {BootstrapComponent, GridState,} from '@solenopsys/ui-templates';
 import {APP_BASE_HREF} from '@angular/common';
 import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, ROUTES_DEV} from "./conf";
 import {ClusterState} from "@solenopsys/fl-clusters";
-import {createNgxs} from "@solenopsys/fl-storage";
 import {RowsState} from "@solenopsys/ui-lists";
-import {environment} from "../environments/environment";
 import {HStreamService, HStreamsState, StreamsPool, WsPool} from "@solenopsys/fl-hyperstreams";
 import {FragmentState} from "../lib/store/fragment.store";
 import {UIEditorModule} from "../lib/ui-editor.module";
 import {ContentNodeState} from "../lib/store/content-node.store";
 import {ContentGroupState} from "../lib/store/content-groups.store";
 import {ContentState} from "../lib/store/content.store";
-import {ParagraphMoveComponent} from "../lib/paragraph-move/paragraph-move.component";
 
+export const STATES=[
+  ClusterState,
+  FragmentState,
+  ContentState,
+  ContentGroupState,
+  ContentNodeState,
+  RowsState,
+  GridState,
+  HStreamsState
+]
 
 @NgModule({
   declarations: [
@@ -23,19 +30,7 @@ import {ParagraphMoveComponent} from "../lib/paragraph-move/paragraph-move.compo
   ],
   imports: [
     RouterModule.forRoot(ROUTES_DEV),
-    ...IMPORTS_CONF,
-    ...createNgxs(!environment.production, [
-      ClusterState,
-      ParagraphMoveComponent,
-      FragmentState,
-      UIEditorModule,
-      ContentState,
-      ContentGroupState,
-      ContentNodeState,
-      RowsState,
-      GridState,
-      HStreamsState
-    ], true),
+    ...IMPORTS_CONF
   ],
   providers: [...PROVIDERS_CONF, WsPool,HStreamService,StreamsPool,
     {provide: 'assets_dir', useValue: ''},
--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -1,20 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, routesAndRedirect} from "./conf";
-import {createNgxs} from "@solenopsys/fl-storage";
-import {environment} from "../environments/environment";
 import {UIEditorModule} from "../lib/ui-editor.module";
 import {FragmentState} from "../lib/store/fragment.store";
 import {ContentState} from "../lib/store/content.store";
 import {ContentGroupState} from "../lib/store/content-groups.store";
 import {ContentNodeState} from "../lib/store/content-node.store";
 
+export const STATES=[FragmentState, ContentState, ContentGroupState, ContentNodeState]
+
 @NgModule({
     //declarations: [...DECLARATION],
     imports: [
         RouterModule.forChild(routesAndRedirect),
         ...IMPORTS_CONF,
-        ...createNgxs(!environment.production, [FragmentState, ContentState, ContentGroupState, ContentNodeState]),
         UIEditorModule
     ],
     providers: [
